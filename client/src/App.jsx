25f2ff34508ec0fe7fa6c3982bc03c1403f85ddb
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2023-05-31 14:06:38 -0700

    Condensed ProductCard, expanded main app height

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,55 +6,61 @@ import LoginPage from './component/LoginPage.jsx';
 import { UserContextProvider } from './UserContext.jsx';
 import { Header } from './Header.jsx';
 import { Meme } from './component/Meme.jsx';
-import Shrek from './shrekProfile.JPG'
+import Shrek from './shrekProfile.JPG';
 
 export default function App() {
   //navbar above h1
   return (
-    <div className="flex flex-col h-screen border-2 border-solid border-orange-400">
-
+    <div /*className="flex flex-col h-screen border-2 border-solid border-orange-400"*/
+    >
       <UserContextProvider>
         <Routes>
-          <Route index element={
-            <main>
-              <Header />
-              <div className="flex items-center justify-center">
-                <p className="text-2xl text-center font-bold">
-                  <strong>
-                  DealDex we find the best deal
-                  </strong>
+          <Route
+            index
+            element={
+              <main className="flex flex-col h-screen border-2 border-solid border-orange-400">
+                <Header />
+                <div className="flex items-center justify-center">
+                  <p className="text-2xl text-center font-bold">
+                    <strong>DealDex we find the best deal</strong>
+                  </p>
+                </div>
+                <Main />
+              </main>
+            }
+          />
+
+          <Route
+            path={'/login'}
+            element={
+              <main>
+                <Header />
+                <LoginPage />
+              </main>
+            }
+          />
+          <Route
+            path={'/register'}
+            element={
+              <main>
+                <Header />
+                <RegisterPage />
+              </main>
+            }
+          />
+          <Route
+            path={'/meme'}
+            element={
+              <main>
+                <Header />
+                <p className="flex items-center justify-center">
+                  <strong>You can do it</strong>
                 </p>
-              </div>
-              <Main />
-            </main>
-          } />
-
-          <Route path={'/login'} element = {
-            <main>
-              <Header />
-              <LoginPage />
-
-            </main>
-           } />
-          <Route path={'/register'} element = {
-            <main>
-              <Header />
-              <RegisterPage />
-
-            </main>
-           } />
-          <Route path={'/meme'} element={
-          <main>
-            <Header />
-            <p className="flex items-center justify-center"><strong>You can do it</strong></p>
-            <img className="flex items-center justify-center" src={Shrek}/>
-          </main>} />
-
+                <img className="flex items-center justify-center" src={Shrek} />
+              </main>
+            }
+          />
         </Routes>
-         
-         
-      
-
 
         {/* <Routes>
 
@@ -63,7 +69,6 @@ export default function App() {
           <Route path={'/login'} element={<LoginPage />} />
  
         </Routes> */}
-
       </UserContextProvider>
     </div>
   );
