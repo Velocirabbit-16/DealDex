afd10ec725fdc70885ceb40d307aeada5a0bb357
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2023-05-31 16:54:09 -0700

    Started work on filtering

--- client/src/container/Main.jsx
+++ client/src/container/Main.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import ProductList from "../component/ProductList.jsx";
-import SearchBar from "../component/SearchBar.jsx";
+import React, { useState } from 'react';
+import ProductList from '../component/ProductList.jsx';
+import SearchBar from '../component/SearchBar.jsx';
 
 export default function Main() {
   // define state variable
-  const [product, setProduct] = useState("");
+  const [product, setProduct] = useState('');
   const [specs, setSpecs] = useState([]); // or {} depends on backend
   //const [filteredHits, setFilteredHits] = useState([]); // or {} depends on backend
   const [userFilters, setUserFilters] = useState({});
@@ -16,24 +16,31 @@ export default function Main() {
 
   const updateFilter = (event, key) => {
     //takes a key (spec), and an input (event.target), and adds it to userFilters
-    const dummyObj = {};
-    dummyObj[key] = event.target.value;
-    const newUserFilters = Object.assign({}, userFilters, dummyObj);
-    console.log("NewUserFilters", newUserFilters);
-    setUserFilters(newUserFilters);
+    const newState = structuredClone(userFilters);
+    if (event.target.value === '') delete newState[key];
+    else newState[key] = event.target.value;
+    console.log('NewUserFilters', newState);
+    setUserFilters(newState);
+  };
+
+  //takes in array of products, returns a filtered array.
+  // iterates through all the filters in the userFilters array.
+  const applyFilters = (arr) => {
+    const filterArr = Object.values(userFilters);
+    return filterArr.reduce((acc, curr) => acc.filter(curr), arr);
   };
 
   const handleSelect = async (event) => {
     const selectedCategory = event.target.value;
-    console.log("selected category ", selectedCategory);
+    console.log('selected category ', selectedCategory);
     setProduct(event.target.value);
 
     try {
-      const response = await fetch("/api/category", {
-        method: "POST",
+      const response = await fetch('/api/category', {
+        method: 'POST',
         body: JSON.stringify({ category: selectedCategory }),
         headers: {
-          "Content-Type": "application/json; charset=UTF-8",
+          'Content-Type': 'application/json; charset=UTF-8',
         },
       });
       const data = await response.json();
@@ -42,17 +49,17 @@ export default function Main() {
       setSpecs(data);
     } catch (error) {
       console.log({
-        log: "handleError",
+        log: 'handleError',
         status: 400,
-        message: "blew up in handleData ",
+        message: 'blew up in handleData ',
         error,
       });
     }
   };
 
   return (
-    <div className='border border-solid border-green-400 flex flex-grow bg-green-100'>
-      <div className='flex flex-col flex-shrink border-solid border border-red-400 w-56'>
+    <div className="border border-solid border-green-400 flex flex-grow bg-green-100">
+      <div className="flex flex-col flex-shrink border-solid border border-red-400 w-56">
         <SearchBar
           handleSelect={handleSelect}
           product={product}
@@ -60,7 +67,7 @@ export default function Main() {
           updateFilter={updateFilter}
         />
       </div>
-      <div className='flex flex-grow border-solid border border-red-400'>
+      <div className="flex flex-grow border-solid border border-red-400">
         <ProductList product={product} specs={specs} />
       </div>
     </div>
